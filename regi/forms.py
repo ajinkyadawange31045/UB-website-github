38802ac28abcc5ff30f58d4edf737d374f853b01
sanjeevholla26 <sanjeevholla26@gmail.com>
2023-04-04 16:03:25 +0530

    changing the signup form

--- regi/forms.py
+++ regi/forms.py
@@ -4,27 +4,28 @@ from django.contrib.auth.models import User
 from django.utils.translation import gettext, gettext_lazy as _
 from .models import Profile
 
+
 # from mptt.forms import TreeNodeChoiceField
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
 class SignUpForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'font-weight: bold; font-weight: bold;'}), help_text='<small class="text-muted">Optional</small>')
-    last_name = forms.CharField(max_length=30, required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'style': 'font-weight: bold;'}), help_text='<small class="text-muted">Optional</small>')
-    email = forms.EmailField(max_length=254, widget=forms.EmailInput(attrs={'class': 'form-control'}), help_text='<small class="text-muted">Enter a valid email address</small>')
+    first_name = forms.CharField(label='', max_length=30, required=False, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder' : 'First Name', 'style': 'font-weight: bold; font-weight: bold;'}), help_text='<small class="text-muted">Optional</small>', )
+    last_name = forms.CharField(label='', max_length=30, required=False, widget=forms.TextInput(attrs={'class': 'form-control','placeholder' : 'Last Name', 'style': 'font-weight: bold;'}), help_text='<small class="text-muted">Optional</small>')
+    email = forms.EmailField(label='', max_length=254, widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder' : 'Email-Id'}), help_text='<small class="text-muted">Enter a valid email address</small>')
 
     # profile_image = forms.ImageField(required=True, widget=forms.FileInput(attrs={'class': 'form-control'}), help_text='<small class="text-danger">Upload a profile picture of less than 2mb</small>')
-    password1 = forms.CharField(label='Password',widget=forms.PasswordInput(attrs={'class':'form-control'}))
-    password2 = forms.CharField(label='Confirm Password(again)',widget=forms.PasswordInput(attrs={'class':'form-control'}))
+    password1 = forms.CharField(label='',widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder' : 'Password'}))
+    password2 = forms.CharField(label='',widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder' : 'Confirm Password(again)'}))
 
     class Meta:
         model = User
         fields = ['username','first_name','last_name','email']
         # fields = ['username','first_name','last_name','email', 'profile_image']
-        labels = {'first_name':'<strong>First Name</strong>','last_name':'<strong>Last Name</strong>','email':'<strong>Email</strong>', 'profile_image': '<strong>Profile Image</strong>'}
+        labels = {'username' : '', 'first_name':'<strong>First Name</strong>','last_name':'<strong>Last Name</strong>','email':'<strong>Email</strong>', 'profile_image': '<strong>Profile Image</strong>'}
 
-        widgets = {'username':forms.TextInput(attrs={'class':'form-control'}),
+        widgets = {'username':forms.TextInput(attrs={'class':'form-control', 'placeholder' : 'Username'}),
         'first_name':forms.TextInput(attrs={'class':'form-control', 'style': 'font-weight: bold;'}),
         'last_name':forms.TextInput(attrs={'class':'form-control', 'style': 'font-weight: bold;'}),
         'email':forms.EmailInput(attrs={'class':'form-control'}),
