7e47f2a48df6dfde513b93dfcd85b8be5acbec97
ajinkyadawange31045 <ajinkyadawange333ard@gmail.com>
2023-04-27 09:41:07 +0530

    post page, resolve media not found error

--- ubwebsite/settings.py
+++ ubwebsite/settings.py
@@ -233,7 +233,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # SESSION_COOKIE_SECURE = False
 # CSRF_COOKIE_SECURE = False
--- ubwebsite/urls.py
+++ ubwebsite/urls.py
@@ -45,6 +45,10 @@ urlpatterns = [
 ] 
 from django.conf import settings
 from django.conf.urls.static import static
-if settings.DEBUG:
+if settings.DEBUG is True:
         urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
         urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
